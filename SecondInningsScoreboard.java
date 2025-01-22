350d482770e8135e42a8f4a462f12fa235e7ea82
CharithJ09 <charithseneviratne1@gmail.com>
2025-01-22 06:17:04 +0530

    Second innings

--- SecondInningsScoreboard.java
+++ SecondInningsScoreboard.java
@@ -19,10 +19,40 @@ public class SecondInningsScoreboard extends FirstInningsScoreboard{
 
     }
 
+    //Calculating the required run rate and the number of balls remaining
     void updateScoreBoard(Decision decision){
+        super.updateScoreBoard(decision);
+        calculateRequiredRunRate();
+        switch(decision){
+            case NO_BALL:
+            case DOT_BALL: break;
+            default:
+                this.ballsRemaining--;
+        }
     }
 
     void displayScoreBoard(){
+
+            // Printing the Scoreboard Header
+            System.out.printf("+-------------------------------------------+%n");
+            System.out.printf("|                SCOREBOARD                 |%n");
+            System.out.printf("+-------------------------------------------+%n");
+
+            // Printing Team Scores
+            System.out.printf("| %19s vs %-18s |%n", battingTeam.teamName,bowlingTeam.teamName);
+            System.out.printf("+-------------------------------------------+%n");
+            System.out.printf("| Over:%6s |%6d - %-20d|%n", over,runsScored,wickets);
+            System.out.printf("| CRR:  %-35.2f |%n", runRate);
+            System.out.printf("+-------------------------------------------+%n");
+
+            // Printing Batsman Scores
+            System.out.printf("| > %-10s - %-3d Runs                   |%n", strikerBatsman.playerName,strikerScore);
+            System.out.printf("|   %-10s - %-3d Runs                   |%n", nonStrikerBatsman.playerName, nonStrikerScore);
+            System.out.printf("+-------------------------------------------+%n");
+
+            // Printing Bowler Details
+            System.out.printf("| %-12s  %-28s|%n",currentBowler.playerName, overProgression);
+            System.out.printf("+-------------------------------------------+%n");
 
     }
 
